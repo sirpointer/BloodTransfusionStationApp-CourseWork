5cb021860ffc3ef7eaa7e95cedf00757754b2e0c
sirpointer <sirpointer@gmail.com>
2018-06-16 14:10:46 +0400

    Fixed search bug

--- BloodTransfusionStationApp/BloodTransfusionStationApp/Controllers/Прием_кровиController.cs
+++ BloodTransfusionStationApp/BloodTransfusionStationApp/Controllers/Прием_кровиController.cs
@@ -40,7 +40,9 @@ namespace BloodTransfusionStationApp.Controllers
                 {
                     прием = прием_крови.Where(i => i.Дата_приема.Day == dateTime.Day
                                                             && i.Дата_приема.Month == dateTime.Month
-                                                            && i.Дата_приема.Year == dateTime.Year).First();
+                                                            && i.Дата_приема.Year == dateTime.Year
+                                                            && i.Дата_приема.Hour == dateTime.Hour
+                                                            && i.Дата_приема.Minute == dateTime.Minute).First();
                 }
                 catch (InvalidOperationException)
                 {
